0e21b5c5d2e8eff320414827229c3c4e0cf84720
AugustoPr🤡 <augusto.rolloff@escola.pr.gov.br>
2026-09-01 18:10:33 -0300

    Add files via upload

---
+++ recuperação/app.js
@@ -0,0 +1,44 @@
+import { View, Text } from 'react-native';
+
+function CartaoProduto({ nome, preco }) {
+  return (
+    <View>
+      <Text>{nome}</Text>
+      <Text>R$ {preco.toFixed(2)}</Text>
+    </View>
+  );
+}
+
+export default function App() {
+  const nome = 'João';
+  const idade = 17;
+  const cidade = 'Cascavel';
+
+  const produtos = [
+    { nome: 'Teclado', preco: 120.00 },
+    { nome: 'Mouse', preco: 60.00 },
+    { nome: 'Monitor', preco: 900.00 }
+  ];
+
+  return (
+    <View>
+      <Text style={{ color: 'blue' }}>{nome}</Text>
+      <Text>{idade} anos</Text>
+      <Text>{cidade}</Text>
+
+      {produtos.map((produto) => (
+        <Text key={produto.nome}>
+          {produto.nome} - R${produto.preco.toFixed(2)}
+        </Text>
+      ))}
+
+      {produtos.map((p) => (
+        <CartaoProduto
+          key={p.nome}
+          nome={p.nome}
+          preco={p.preco}
+        />
+      ))}
+    </View>
+  );
+}
